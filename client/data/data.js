24f6ae32ca0f74be79f8de594bfd054ca3a90386
Hack Reactor Students <accounts@hackreactor.com>
2015-09-03 10:45:59 -0700

    Added Play counter and increased library size

--- client/data/data.js
+++ client/data/data.js
@@ -19,5 +19,45 @@ var songData = [
     url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
     title: "If Your Girl Only Knew",
     artist: "Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
+    title: "Song1",
+    artist: "Not Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
+    title: "Song2",
+    artist: "Not Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
+    title: "Song3",
+    artist: "Not Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+    title: "Song4",
+    artist: "Not Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
+    title: "Song-A",
+    artist: "Better than Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
+    title: "Song-B",
+    artist: "Better than Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
+    title: "Song-C",
+    artist: "Better than Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+    title: "Song-D",
+    artist: "Better than Aaliyah",
   }
 ];
